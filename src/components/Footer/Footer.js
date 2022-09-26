3fd2e1a99844e618adecd8985e691eb19520d796
HazratAli <hazrat369ali@gmail.com>
2022-09-26 06:10:07 +0600

    confrim

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='text-center bg-light p-4'>
+            <h5 className='m-0'>Md. Hazrat Ali - 2022</h5>
+        </div>
+    );
+};
+
+export default Footer;
